98efdcb202a65d24557fa4ef7bae1584f839a085
Kisu <kisuzoid@gmail.com>
2023-07-25 19:05:28 +0530

    gross calculator modified

--- C/gross_salary.c
+++ C/gross_salary.c
@@ -9,7 +9,39 @@ int main()
     printf("Input Your Base Salary: ");
     scanf("%i",&x);
 
+    int p;
+    printf("Enter Dearness Allowence in percentage of your Base Salary: ");
+    scanf("%i",&p);
+    int k = p*x/100;
+
+    int q;
+    printf("Enter Rent Allowence in percentage of your Base Salary: ");
+    scanf("%i",&q);
+    int l = q*x/100;
+
+    int y = x + k + l;
+    printf("%i",y);
+    return 0;
+
+}
+
+
+
+
+
+
+
+
+
+/*
+int main()
+{
+    int x;
+    printf("Input Your Base Salary: ");
+    scanf("%i",&x);
+
     int y = x + 0.2*x + 0.4*x;
     printf("%i",y);
     return 0;
 }
+*/
