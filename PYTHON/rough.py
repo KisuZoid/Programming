6c9c7dc64a009154b3af248d319950bb71cbe55a
Kisu <kisuzoid@gmail.com>
2023-07-26 11:53:32 +0530

    modified

--- PYTHON/rough.py
+++ PYTHON/rough.py
@@ -1,4 +1,8 @@
-message = " Python "
+message =  " Programming Languages:\n\tC\n\tPython\n\tJAVA\n\tJS  "
 message = message.strip().upper()
-#message = message.upper()
+
 print(message)
+
+#message = ' Python '
+#message = message.upper()
+#message = message.strip()
