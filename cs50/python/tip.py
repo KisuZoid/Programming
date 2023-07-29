2e14190122fbc0c64d3ceecb80a83e733d0cc9f8
Kisu <kisuzoid@gmail.com>
2023-07-29 23:35:08 +0530

    modified

--- cs50/python/tip.py
+++ cs50/python/tip.py
@@ -1,5 +1,7 @@
 #tip calculator
-
+#dollar_to_float, which accept a str as input as formate $##.##, where # is a decimal digit, remove $ and return the amount as a float i.e. $50.00 means 50.0
+#percent_to_float, which accept a str as input as formate ##%, where # is a decimal digit, remove % and return the percentage as a float i.e. 15% means 0.15
+    
 def main():
     dollars = dollar_to_float(input("How much was the meal? "))
     percentage = percent_to_float(input("What percentage would you like to tip? "))
@@ -42,5 +44,3 @@ def percent_to_float():
 main()
 """
 
-#dollar_to_float, which accept a str as input as formate $##.##, where # is a decimal digit, remove $ and return the amount as a float i.e. $50.00 means 50.0
-#percent_to_float, which accept a str as input as formate ##%, where # is a decimal digit, remove % and return the percentage as a float i.e. 15% means 0.15
