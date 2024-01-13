8e2392f8c247cfa10e36c4dacb01f9f2083606bb
Kisu <kisuzoid@gmail.com>
2024-01-13 14:25:21 +0530

    deleted

--- cs50/Py/indoor.py
+++
@@ -1,6 +0,0 @@
-#take input from user
-x = input()
-#convert that input in all lowercase
-x = x.lower()
-#print the same input in all lowercase
-print(x)
--- cs50/Py/playback.py
+++
@@ -1,7 +0,0 @@
-#input from user.
-input = input()
-#output that same input, replacing each space with ...
-input = input.replace(" ", "...")
-print(input)
-
-#replace() function replaces values i.e. replace("1","2") means replace 1 with 2.
